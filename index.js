84dbb0f9cb949e88cf6023567f5efefc762f6cbf
stackpearson <60953368+stackpearson@users.noreply.github.com>
2020-02-11 20:26:51 -0700

    Create index.js
    java script for 1st web page

---
+++ index.js
@@ -0,0 +1,12 @@
+// define the function to operate your button here 
+
+function showBella(){
+    document.getElementById('bella-pic').style.display="block";
+    document.getElementById('hide-button').style.display="block";
+    document.getElementById('show-button').style.display="none";
+    }
+function hideBella(){
+    document.getElementById('bella-pic').style.display="none";
+    document.getElementById('show-button').style.display="block";
+    document.getElementById('hide-button').style.display="none";
+}
